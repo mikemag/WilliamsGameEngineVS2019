b97946b417465462cf9c85063b14968fc62bb009
Mike Magruder <mikemag@outlook.com>
2021-02-05 00:05:23 -0800

    Part 4

--- Base project/MyGame/MyGame/GameScene.cpp
+++ Base project/MyGame/MyGame/GameScene.cpp
@@ -1,7 +1,11 @@
 #include "GameScene.h"
 #include "Ship.h"
+#include "Meteor.h"
 
 GameScene::GameScene() {
   ShipPtr ship = std::make_shared<Ship>();
   addGameObject(ship);
+
+  MeteorPtr m = std::make_shared<Meteor>(sf::Vector2f(400, 100));
+  addGameObject(m);
 }
